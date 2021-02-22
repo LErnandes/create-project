3841df10eb1d6256cb60825f932c00f99b729190
unknown <luis78270@gmail.com>
2021-02-22 13:13:24 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# `@ernandes/create-project`
+
+🏗 A CLI to templates
+
+## Installation & Usage
+
+```bash
+npm install -g @ernandes/create-project
+create-project
+```
+
+## License
+
+MIT
+
+## Collaborators
+
+- Luis Ernandes <luis78270@gmail.com>
---
+++ templates/node-auth/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const MONGOURI = process.env.MONGOURI;
+
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true
+    });
+    console.log("Connected to DB !!");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+module.exports = InitiateMongoServer;
